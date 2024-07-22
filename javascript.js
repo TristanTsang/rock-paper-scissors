5f7cd8463fe7bc9aa47f93fcf698b04edb52da53
Tristan Tsang <tristantsang9786@gmail.com>
2024-07-22 17:06:20 -0400

    Replace .equals() with ===

--- javascript.js
+++ javascript.js
@@ -16,36 +16,36 @@ function playRound() {
   humanChoice = getHumanChoice();
   computerChoice = getComputerChoice();
 
-  if (computerChoice.equals("rock")) {
-    if (playerChoice.equals("rock")) {
+  if (computerChoice === "rock") {
+    if (playerChoice === "rock") {
       console.log("You both chose rock. It's a draw!");
     }
-    if (playerChoice.equals("paper")) {
+    if (playerChoice === "paper") {
       console.log("You chose paper; the computer chose rock. You win!");
       playerScore++;
     }
-    if (playerChoice.equals("scissors")) {
+    if (playerChoice === "scissors") {
       console.log("You chose scissors; the computer chose rock. You lose!");
       computerScore++;
     }
-  } else if (computerChoice.equals("paper")) {
-    if (playerChoice.equals("rock")) {
+  } else if (computerChoice === "paper") {
+    if (playerChoice === "rock") {
       console.log("You chose rock; the computer chose paper. You lose!");
       computerScore++;
-    } else if (playerChoice.equals("paper")) {
+    } else if (playerChoice === "paper") {
       console.log("You both chose paper. It's a draw!");
-    } else if (playerChoice.equals("scissors")) {
+    } else if (playerChoice === "scissors") {
       console.log("You chose scissors; the computer chose paper. You win!");
       playerScore++;
     }
-  } else if (computerChoice.equals("scissors")) {
-    if (playerChoice.equals("rock")) {
+  } else if (computerChoice === "scissors") {
+    if (playerChoice === "rock") {
       console.log("You chose rock; the computer chose scissors. You win!");
       playerScore++;
-    } else if (playerChoice.equals("paper")) {
+    } else if (playerChoice === "paper") {
       console.log("You chose paper; the computer chose scissors. You lose!");
       computerScore++;
-    } else if (playerChoice.equals("scissors")) {
+    } else if (playerChoice === "scissors") {
       console.log("You both chose scissors. It's a draw!");
     }
   }
